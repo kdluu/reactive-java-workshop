f80f61d74af1e2bbe72b25373e1d0753b023987e
Khanh Luu <khanhluu101@gmail.com>
2025-01-20 13:03:15 -0500

    Completed exercise 5

--- src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
@@ -1,5 +1,8 @@
 package io.javabrains.reactiveworkshop;
 
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+
 import java.io.IOException;
 
 public class Exercise5 {
@@ -9,13 +12,48 @@ public class Exercise5 {
         // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
 
         // Subscribe to a flux using the error and completion hooks
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().subscribe(
+                number -> System.out.println(number),
+                error -> System.out.println(error.getMessage()),
+                () -> System.out.println("Completed")
+        );
+//         Subscribe to a flux using an implementation of BaseSubscriber
+        ReactiveSources.intNumbersFlux().subscribe(new BaseSubscriber<Integer>() {
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                super.hookOnSubscribe(subscription);
+                System.out.println("Subscribed");
+            }
 
-        // Subscribe to a flux using an implementation of BaseSubscriber
-        // TODO: Write code here
+            @Override
+            protected void hookOnNext(Integer value) {
+                super.hookOnNext(value);
+                System.out.println("value: " + value);
+            }
+
+            @Override
+            protected void hookOnComplete() {
+                super.hookOnComplete();
+                System.out.println("Completed");
+            }
+        });
+
+        ReactiveSources.intNumbersFlux().subscribe(new MySubscriber<>());
 
         System.out.println("Press a key to end");
         System.in.read();
     }
 
+    static class MySubscriber<T> extends BaseSubscriber<T> {
+
+        public void hookOnSubscribe(Subscription subscription) {
+            System.out.println("Subscribe happened");
+            request(1);
+        }
+
+        public void hookOnNext(T value) {
+            System.out.println(value.toString() + " received");
+            request(3);
+        }
+    }
 }
